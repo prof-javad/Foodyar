b0ac0793b309ac7d08b09740dde3ae1c26a623b2
Javad <prof.mj007@gmail.com>
2026-05-26 07:13:33 +0330

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,64 +6,53 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
 
   getFirestore,
-
   collection,
-
   addDoc,
-
   getDocs,
-
   deleteDoc,
-
   updateDoc,
-
-  doc
+  doc,
+  onSnapshot,
+  enableIndexedDbPersistence,
+  enableNetwork,
+  disableNetwork,
+  CACHE_SIZE_UNLIMITED
 
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-  apiKey:
-  "AIzaSyCYM1hZ94IPgGxIbkzrUB3Lgz9WKJ08a9Y",
-
-  authDomain:
-  "foodyar-bb4ad.firebaseapp.com",
-
-  projectId:
-  "foodyar-bb4ad",
-
-  storageBucket:
-  "foodyar-bb4ad.firebasestorage.app",
-
-  messagingSenderId:
-  "802950674478",
-
-  appId:
-  "1:802950674478:web:63a336fc78ce53357d87bf"
-
+  apiKey: "AIzaSyCYM1hZ94IPgGxIbkzrUB3Lgz9WKJ08a9Y",
+  authDomain: "foodyar-bb4ad.firebaseapp.com",
+  projectId: "foodyar-bb4ad",
+  storageBucket: "foodyar-bb4ad.firebasestorage.app",
+  messagingSenderId: "802950674478",
+  appId: "1:802950674478:web:63a336fc78ce53357d87bf"
 };
 
-const app =
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-const db =
-getFirestore(app);
+// فعال کردن قابلیت آفلاین (ذخیره در IndexedDB)
+enableIndexedDbPersistence(db, {
+  cacheSizeBytes: CACHE_SIZE_UNLIMITED
+}).catch((err) => {
+  if (err.code === 'failed-precondition') {
+    console.warn('Multiple tabs open, persistence can only be enabled in one tab at a time.');
+  } else if (err.code === 'unimplemented') {
+    console.warn('The current browser does not support offline persistence');
+  }
+});
 
 export {
-
   db,
-
   collection,
-
   addDoc,
-
   getDocs,
-
   deleteDoc,
-
   updateDoc,
-
-  doc
-
+  doc,
+  onSnapshot,
+  enableNetwork,
+  disableNetwork
 };
